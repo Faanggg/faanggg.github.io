1f5a818c397874c1e4e0bc2651a4d526dbb94c0c
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2023-08-06 22:42:19 +0800

    Added quiz

--- webdeva2/script.js
+++ webdeva2/script.js
@@ -2,10 +2,11 @@
 const page1Button = document.querySelector("#page1Button");
 const page2Button = document.querySelector("#page2Button");
 const page3Button = document.querySelector("#page3Button");
+const page4Button = document.querySelector("#page4Button");
 var allpages = document.querySelectorAll(".page");
 
 hideAllPages();
-showPage(1);
+showPage(4);
 function hideAllPages() { //function to hide all pages
 	for (let onepage of allpages) { //go through all subtopic pages
 		onepage.style.display = "none"; //hide it
@@ -28,6 +29,9 @@ page2Button.addEventListener("click", function() {
 });
 page3Button.addEventListener("click", function() {
 	showPage(3);
+});
+page4Button.addEventListener("click", function() {
+	showPage(4);
 });
 
 const hamBtn = document.querySelector("#hamIcon");
@@ -118,7 +122,7 @@ function exitFullscreen() {
 }
 
 const fullscreenButton = document.querySelector("#fullscreen");
-var isFullscreen = "false";
+let isFullscreen = "false";
 
 fullscreenButton.addEventListener("click", toggleFullscreen);
 
@@ -133,6 +137,137 @@ function toggleFullscreen() {
 	}
 }
 
+const questions = [
+	{
+		question: "What was the largest battleship ever built?",
+		answers: [
+			{text: "Yamato", correct: true},
+			{text: "Bismarck", correct: false},
+			{text: "USS Iowa", correct: false},
+			{text: "Satsuma", correct: false},
+		]
+	},
+	{
+		question: "What did the Washington Naval Treaty restrict about the construction of battleships?",
+		answers: [
+			{text: "Height of a battleship", correct: false},
+			{text: "Shape of a battleship", correct: false},
+			{text: "Speed of a battleship", correct: false},
+			{text: "Size of a battleship", correct: true},
+		]
+	},
+	{
+		question: "What was the first \"all-big-gun\" battleship??",
+		answers: [
+			{text: "Satsuma", correct: false},
+			{text: "HMS Dreadnought", correct: true},
+			{text: "Kii", correct: false},
+			{text: "USS Texas", correct: false},
+		]
+	},
+	{
+		question: "What was the ship that started the Dreadnought era of battleships?",
+		answers: [
+			{text: "Napolean", correct: false},
+			{text: "HMS Dreadnought", correct: true},
+			{text: "Bismarck", correct: false},
+			{text: "Vittorio Veneto", correct: false},
+		]
+	},
+	{
+		question: "Which nations caused America to push for the Washington Naval Treaty to be signed?",
+		answers: [
+			{text: "Britain and Japan", correct: true},
+			{text: "Britain and Germany", correct: false},
+			{text: "Japan and Italy", correct: false},
+			{text: "Japan and France", correct: false},
+		]
+	},
+];
 
+const questionElement = document.querySelector("#question");
+const answerButtons = document.querySelector("#answerButtons");
+const nextButton = document.querySelector("#nextButton");
 
+let currentQuestionNo = 0;
+let score = 0;
 
+function startQuiz() {
+	currentQuestionNo = 0;
+	score = 0;
+	nextButton.innerHTML = "Next";
+	showQuestion();
+}
+
+function showQuestion() {
+	resetState();
+	let currentQuestion = questions[currentQuestionNo];
+	let questionNo = currentQuestionNo + 1;
+	questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+	
+	currentQuestion.answers.forEach(answer => {
+		const ansButton = document.createElement("button");
+		ansButton.innerHTML = answer.text;
+		ansButton.classList.add("quizButton");
+		answerButtons.appendChild(ansButton);
+		if (answer.correct) {
+			answerButtons.dataset.correct = answer.correct;
+		}
+		ansButton.addEventListener("click", selectAnswer);
+	});
+}
+
+function resetState() {
+	nextButton.style.display = "none";
+	while (answerButtons.firstChild) {
+		answerButtons.removeChild(answerButtons.firstChild);
+	}
+}
+
+function selectAnswer(e) {
+	const selectedButton = e.target;
+	const isCorrect = selectedButton.dataset.correct === "true";
+	console.log(isCorrect);
+	if (isCorrect) {
+		selectedButton.classList.add("correct");
+		score++;
+	}
+	else if (!isCorrect) { 
+		selectedButton.classList.add("incorrect");
+	}
+	Array.from(answerButtons.children).forEach(ansButton => {
+		if (ansButton.dataset.correct === "true") {
+			ansButton.classList.add("correct");
+		}
+		ansButton.disabled = true;
+	});
+	nextButton.style.display = "block";
+}
+
+function showScore() {
+	resetState();
+	questionElement.innerHTML = `You scored ${score} out of ${questions.length}`;
+	nextButton.innerHTML = "Play Again";
+	nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+	currentQuestionNo++;
+	if (currentQuestionNo < questions.length) {
+		showQuestion();
+	}
+	else {
+		showScore();
+	}
+}
+
+nextButton.addEventListener("click", ()=>{
+	if (currentQuestionNo < questions.length) {
+		handleNextButton();
+	}
+	else {
+		startQuiz();
+	}
+});
+
+startQuiz();
